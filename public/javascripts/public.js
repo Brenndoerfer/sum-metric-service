ab18f1c507a876a0c4d65df6293b869af85035c6
Michael Brenndoerfer <michael.brenndoerfer@gmail.com>
2018-07-16 23:50:40 -0700

    fix push

--- public/javascripts/public.js
+++ public/javascripts/public.js
@@ -1,9 +1,33 @@
-$.ajax({
-    method: 'get',
-    url: '/demo',
-    data: {}
-}).done(function (res) {
-    alert(res);
-}).fail(function (e) {
-    console.error(e);
+$(document).ready(function () {
+    // addValue('active_visitors', 4);
+    // addValue('active_visitors', 3);
+    // addValue('active_visitors', 7);
+    addValue('active_visitors', 2);
+    addValue('test_me', 2);
+    // addValue('test_me', 5);
+    getSum('active_visitors');
+    getSum('test_me');
 });
+function getSum(metricKey) {
+    $.ajax({
+        method: 'get',
+        url: "/metric/" + metricKey + "/sum",
+    }).done(function (res) {
+        var result = "<li>GET /metric/" + metricKey + "/sum [Result: " + res + "]</li>";
+        $('.get').append(result);
+    }).fail(function (e) {
+        console.error("Failed calling the metric endpoint");
+    });
+}
+function addValue(metricKey, value) {
+    $.ajax({
+        method: 'post',
+        url: "/metric/" + metricKey,
+        data: { value: value }
+    }).done(function (res) {
+        var result = "<li>POST /metric/" + metricKey + " {value:" + value + "}</li>";
+        $('.post').append(result);
+    }).fail(function (e) {
+        console.error("Failed calling the metric endpoint");
+    });
+}
--- public/javascripts/public.ts
+++ public/javascripts/public.ts
@@ -1,11 +1,41 @@
 declare var $: any;
 
-$.ajax({
-    method: 'get',
-    url: '/demo',
-    data: {}
-}).done(res => {
-    alert(res);
-}).fail(e => {
-    console.error(e);
+
+$(document).ready(function () {
+    // addValue('active_visitors', 4);
+    // addValue('active_visitors', 3);
+    // addValue('active_visitors', 7);
+    addValue('active_visitors', 2);
+    addValue('test_me', 2);
+    // addValue('test_me', 5);
+
+    getSum('active_visitors');
+    getSum('test_me');
 });
+
+function getSum(metricKey: string) {
+    $.ajax({
+        method: 'get',
+        url: `/metric/${metricKey}/sum`,
+    }).done(res => {
+        let result: string = `<li>GET /metric/${metricKey}/sum [Result: ${res}]</li>`;
+        $('.get').append(result);
+
+    }).fail(e => {
+        console.error(`Failed calling the metric endpoint`);
+    });
+}
+
+function addValue(metricKey: string, value: number) {
+    $.ajax({
+        method: 'post',
+        url: `/metric/${metricKey}`,
+        data: {value}
+    }).done(res => {
+        let result: string = `<li>POST /metric/${metricKey} {value:${value}}</li>`;
+        $('.post').append(result);
+
+    }).fail(e => {
+        console.error(`Failed calling the metric endpoint`);
+    });
+}
